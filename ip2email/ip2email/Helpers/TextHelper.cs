89277841832976cc5381d909adb8717dbc7d34e1
Inectic <50652175+Inestic@users.noreply.github.com>
2021-03-11 10:37:36 +0300

    Update TextHelper.cs

--- ip2email/ip2email/Helpers/TextHelper.cs
+++ ip2email/ip2email/Helpers/TextHelper.cs
@@ -10,7 +10,7 @@ namespace IP2Email.Helpers
         internal static readonly string FailedGetLocalIP = "Failed to obtain local IP address";
         internal static readonly string InternetIpSite = "https://ifconfig.me/ip";
         internal static readonly string RegistryAppName = "IP2Email";
-        internal static readonly string RegistryKeyAuthor = "oZZo";
+        internal static readonly string RegistryKeyAuthor = "Inestic";
         internal static readonly string RegistryKeySoftware = "Software";
         internal static readonly string SecurityKey = "";
         internal static readonly string SenderDisplayName = "IP2Email";
